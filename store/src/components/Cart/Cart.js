057c7cb212fca0b3e86bf98515c2f1adaf79591c
Marta <lagiewka.marta.dev@gmail.com>
2019-07-30 20:33:45 +0200

    Add CartList and CartItem components

--- store/src/components/Cart/Cart.js
+++ store/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Title from "../Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
+import CartList from "./CartList";
 import { ProductConsumer } from "../../context";
 export default class Cart extends Component {
   render() {
@@ -17,6 +18,7 @@ export default class Cart extends Component {
                 <>
                   <Title name="your" title="cart" />
                   <CartColumns />
+                  <CartList value={value}/>
                 </>
               );
             }
